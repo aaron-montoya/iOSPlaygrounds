2bec9fd15458effe8ce79658e85ecf615325680e
Aaron Montoya <aatoya7@gmail.com>
2017-09-25 12:21:56 -0600

    Trying string operators, such as string changing, interpolation, counting and comparing.

--- Starting.playground/Contents.swift
+++ Starting.playground/Contents.swift
@@ -2,4 +2,20 @@
 
 import UIKit
 
-var str = "Hello, playground"
+//String changing
+var greeting = "Hello"
+greeting = "Salutations"
+
+//String interpolation
+print("\(greeting), my friend.")
+
+//Counting characters
+var num_of_letters = greeting.characters.count
+print(num_of_letters)
+
+//Comparing strings
+if num_of_letters > 5 {
+    print("There's more than 5 letters in the greeting.")
+} else {
+    print("There's less than 5 letters in the greeting.")
+}
