51a57d61ab03b17ddc57c6e55967800e618da9d3
Aaron Montoya <aatoya7@gmail.com>
2017-09-27 12:13:53 -0600

    Wrote a method with a return type, wrote and if/else statement and a while loop

--- MethodsAndStuffs.playground/Contents.swift
+++ MethodsAndStuffs.playground/Contents.swift
@@ -8,7 +8,7 @@ public func isReallySimple() -> Void
 }
 isReallySimple()
 
-public func aBitLessSimple(name :String) -> Void
+public func aBitLessSimple(name:String) -> Void
 {
     let answer = "My name is not " + name
     print(answer)
@@ -16,3 +16,40 @@ public func aBitLessSimple(name :String) -> Void
 aBitLessSimple(name: "Trix")
 let words = "Bilbo"
 aBitLessSimple(name: words)
+
+public func namingExample(outerName innerName:String) -> Void
+{
+    print("The inner name is: \(innerName)")
+}
+namingExample(outerName:"Aaron")
+
+public func counting() -> Int
+{
+    return 7
+}
+print("I am not \(counting()) years old")
+
+if counting() < 10
+{
+    print("Math works in Swift")
+}
+else
+{
+    print("Hopefully this won't print")
+}
+
+var test = 0
+
+while test < counting()
+{
+    print("Hahaha")
+    test += 1
+}
+
+
+
+
+
+
+
+
